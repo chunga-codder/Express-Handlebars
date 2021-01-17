36e3920502946bb5d27315e2e5488287b7bd8897
chunga <clintonchunga@gmail.com>
2021-01-17 11:40:52 +0100

    changing some few parts on my connections

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,29 @@
-var mysql = require('mysql');
+// Set up MySQL connection
+var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Prinsclint1.',
-    database: 'burgers_db1'
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "Prinsclint1.",
+    database: "burgers_db"
   });
 };
 
+
+// Make connection
+
 connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
+//Export connection for our ORM to use
 module.exports = connection;
